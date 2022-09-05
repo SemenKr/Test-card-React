26b4457d69c7c3f3cba382357d8bc69981f557fb
Семён Крекотун <97591773+SemenKr@users.noreply.github.com>
2022-09-05 16:46:27 +0300

    feat: стилизует product-page

--- src/product-page/product-page.jsx
+++ src/product-page/product-page.jsx
@@ -1,40 +1,56 @@
 import React from "react";
 import Title from "/src/title/title";
 import Article from "/src/article/article";
-import Gallery from "/src/gallery/gallery";
-import FullPrice from "/src/full-price/full-price";
-import Count from "/src/count/count";
 import Description from "/src/description/description";
 import Comments from "/src/comments/comments";
 import Popularity from "/src/popularity/popularity";
-import Button from "/src/button/button";
+import { Image } from "/src/elements";
+import {
+  StyledProductPage,
+  Header,
+  ProductWrapper,
+  ProductInfo,
+  ProductInfoLine,
+  PageCounter,
+  BuyButton,
+  PageFullPrice,
+  DeliveryValue
+} from "./styled";
 
 function ProductPage({ product }) {
   return (
-    <section>
-      <Title>{product.name}</Title>
-      <Article>{product.article}</Article>
-      <div style={{ display: "flex" }}>
-        <Gallery src={product.src} alt={product.alt} />
-        <div>
-          <p>
+    <StyledProductPage>
+      <Header>
+        <Title>{product.name}</Title>
+        <Article>{product.article}</Article>
+      </Header>
+      <ProductWrapper>
+        <Image
+          width="200"
+          height="257"
+          maxWidth="200"
+          src={product.src}
+          alt={product.name}
+        />
+        <ProductInfo>
+          <ProductInfoLine>
             Цена:{" "}
-            <FullPrice oldPrice={product.oldPrice} price={product.price} />
-          </p>
-          <div>
-            Количество:
-            <Count />
-          </div>
-          <p>
-            <span>Доставка:</span> {product.delivery}
-          </p>
-          <Button size="large">Купить</Button>
+            <PageFullPrice oldPrice={product.oldPrice} price={product.price} />
+          </ProductInfoLine>
+          <ProductInfoLine>
+            Количество: Количество: <PageCounter />
+          </ProductInfoLine>
+          <ProductInfoLine>
+            <span>Доставка:</span>{" "}
+            <DeliveryValue>{product.delivery}</DeliveryValue>
+          </ProductInfoLine>
+          <BuyButton size="large">Купить</BuyButton>
           <Popularity count={product.comments.length} />
-        </div>
-      </div>
+        </ProductInfo>
+      </ProductWrapper>
       <Description text={product.description} />
       <Comments comments={product.comments} />
-    </section>
+    </StyledProductPage>
   );
 }
 
